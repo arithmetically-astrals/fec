2b59170ca1c0ce25b0d74a22379124b94d50aec5
Casey Penk <caseypenk@gmail.com>
2022-08-25 20:05:21 -0400

    spin up server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,14 @@ import Overview from "./Overview/Overview.jsx";
 import QA from "./QA/QA.jsx";
 import Related from "./Related/Related.jsx";
 import Reviews from "./Reviews/Reviews.jsx";
+import DarkModeToggle from "./Shared/DarkModeToggle.jsx";
 
 const root = createRoot(document.getElementById("root"));
 
 // Huzzah for jsx!
 const App = () => {
   return <div id='appContainer'>
+    <DarkModeToggle />
     <Overview id='overview' />
     <Related id='related' />
     <QA id='qa' />
