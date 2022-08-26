dba66596292fe37a3bfea95c8280f4b89642c1f7
Joseph <josephccchou@gmail.com>
2022-08-26 12:11:34 -0400

    add new state search and pass as prop to Search component

--- client/src/QA/QA.jsx
+++ client/src/QA/QA.jsx
@@ -8,11 +8,12 @@ const QA = () => {
 
   const [questions, setQuestions] = useState([{text: 'This is a question', answers: ['This is an answer', 'and so is this']}]);
   const [moreQuestions, setMoreQuestions] = useState(false);
+  const [search, setSearch] = useState('');
 
   return (
     <div>
       <h1>Questions</h1>
-      <Search />
+      <Search search={search} setSearch={setSearch}/>
       <QuestionList questions={questions}/>
       <MoreQuestions moreQuestions={moreQuestions} setMoreQuestions={setMoreQuestions}/>
       <AddQuestion questions={questions} setQuestions={setQuestions}/>
