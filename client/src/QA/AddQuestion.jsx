6212047e5cde677484825a6a38a2e3b89a260403
Joseph <josephccchou@gmail.com>
2022-08-26 11:17:05 -0400

    add temporary input html that uses setText to change text state

--- client/src/QA/AddQuestion.jsx
+++ client/src/QA/AddQuestion.jsx
@@ -1,9 +1,21 @@
 import React, {useState} from "react";
 
-const AddQuestion = (props) => (
-  <button onClick={() => {
-    props.setQuestions([{text: 'testing!', answers: ['answertesting']}]);
-  }}>Add a question</button>
-)
+const AddQuestion = (props) => {
+
+  const [text, setText] = useState ('');
+
+  return (
+    <>
+      <input type={text} placeholder="temporary input" onChange={(e) => {
+        setText(e.target.value);
+        console.log(text);
+      }}/>
+      <button onClick={() => {
+        props.setQuestions([{text: 'testing!', answers: ['answertesting']}]);
+      }}>Add a question</button>
+    </>
+  )
+
+}
 
 export default AddQuestion;
