95b244f100c3407e540fcbfff7734cf09b26f00d
Joseph <josephccchou@gmail.com>
2022-08-25 22:15:34 -0400

    add map method to QuestionList and add hardcoded data in QA

--- client/src/QA/QA.jsx
+++ client/src/QA/QA.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import Search from "./Search.jsx";
 import QuestionList from "./QuestionList.jsx";
 import MoreQuestions from "./MoreQuestions.jsx";
 import AddQuestion from "./AddQuestion.jsx";
 
-const QA = () => (
-  <div>
-    <h1>Questions</h1>
-    <Search />
-    <QuestionList />
-    <MoreQuestions />
-    <AddQuestion />
-  </div>
-)
+const QA = () => {
+
+  const [questions, setQuestions] = useState([{question: '??'}]);
+
+  return (
+    <div>
+      <h1>Questions</h1>
+      <Search />
+      <QuestionList questions={questions}/>
+      <MoreQuestions />
+      <AddQuestion />
+    </div>
+  )
+}
 
 export default QA;
--- client/src/QA/QuestionList.jsx
+++ client/src/QA/QuestionList.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const QuestionList = () => (
-  <div>QuestionList</div>
+const QuestionList = (props) => (
+  <div>
+    {props.questions.map((question, index) => (
+      <div key={index}>!!!</div>
+    ))}
+  </div>
 )
 
 export default QuestionList;
