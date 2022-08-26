491d2d056281a7d4e0ee75cddd376b3156d7e29b
Joseph <josephccchou@gmail.com>
2022-08-26 16:46:21 -0400

    add info bar underneath each answer and minor refactoring of answerList

--- client/src/QA/Answer.jsx
+++ client/src/QA/Answer.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const Answer = (props) => (
-  <>{props.answer}</>
+  <>
+    <>{props.answer.body}</>
+    <div>by {props.answer.answerer_name} | Helpful? ({props.answer.helpfulness}) | Report</div>
+  </>
 )
 
 export default Answer;
