e31d77c2e7bb9babc596ab7ea7173c348502bba0
Brian <BRVose@gmail.com>
2022-08-26 12:11:58 -0600

    added some requests in server/index.js to the API

--- server/index.js
+++ server/index.js
@@ -12,8 +12,55 @@ app.use(express.static(path.join(__dirname, '../client/public')));
 app.use(express.json());
 
 //requests
-app.get('/reviews', (req, res) => {
 
+//gets all products
+app.get('/products', (req, res) => {
+  axios.get(`${process.env.API}/products`, {
+    headers: {
+      'Authorization': process.env.AUTH_CODE
+    }
+  }).then(response => {
+    res.status(200);
+    res.send(response.data);
+  }).catch(err => {
+    console.log(err)
+    res.sendStatus(404);
+  })
+});
+
+
+//gets all reviews for item
+app.get('/reviews', (req, res) => {
+  var idNum = req.params.product_id;
+  axios.get(`${process.env.API}/reviews/`, {
+    headers: {
+      'Authorization': process.env.AUTH_CODE
+    },
+    params: { product_id: idNum }
+  }).then(response => {
+    res.status(200);
+    res.send(response.data);
+  }).catch(err => {
+    console.log(err)
+    res.sendStatus(404);
+  })
+});
+
+//gets all meta review info for item
+app.get('/reviews/meta', (req, res) => {
+  var idNum = req.params.product_id;
+  axios.get(`${process.env.API}/reviews/meta`, {
+    headers: {
+      'Authorization': process.env.AUTH_CODE
+    },
+    params: { product_id: idNum }
+  }).then(response => {
+    res.status(200);
+    res.send(response.data);
+  }).catch(err => {
+    console.log(err)
+    res.sendStatus(404);
+  })
 });
 
 
