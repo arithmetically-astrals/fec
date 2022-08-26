36a36621d3ed7587c9918a12bb6298b37ae291ba
Brian <BRVose@gmail.com>
2022-08-26 08:53:04 -0600

    minor changes to review files

--- client/src/reviews/ReviewMenu.jsx
+++ client/src/reviews/ReviewMenu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 // Huzzah for jsx!
 const ReviewMenu = () => {
-  return <h1>This is the review menu!</h1>
+  return (<h1>This is the review menu!</h1>)
 }
 
 export default ReviewMenu;
