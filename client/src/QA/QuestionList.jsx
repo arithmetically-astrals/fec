5c6eb83256f9f30094c5c8a4fb825b33b0c725c1
Joseph <josephccchou@gmail.com>
2022-08-29 12:09:30 -0400

    render searched questions

--- client/src/QA/QuestionList.jsx
+++ client/src/QA/QuestionList.jsx
@@ -4,7 +4,11 @@ import Question from "./Question.jsx";
 const QuestionList = (props) => (
   <div>
     {props.search.length >= 3
-    ? <div>hello</div>
+    ? props.questions.map((question, index) => {
+      if (question.question_body.includes(props.search)) {
+        return <Question question={question} key={index}/>
+      }
+    })
     : props.questions.map((question, index) => (
       <Question question={question} key={index}/>
     ))}
