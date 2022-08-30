06d38d34059ff9e82725f925140497f04736857d
Joseph <josephccchou@gmail.com>
2022-08-29 21:09:16 -0400

    fix bug with rendering no questions found

--- client/src/QA/QuestionList.jsx
+++ client/src/QA/QuestionList.jsx
@@ -3,24 +3,21 @@ import Question from "./Question.jsx";
 
 const QuestionList = (props) => (
   <div>
-    {props.search.length >= 3
-    ? props.questions.map((question, index) => {
-      let emptyRender = true;
-      if (question.question_body.includes(props.search)) {
-        emptyRender = false;
-        return <Question question={question} key={index}/>
-      }
-      if (emptyRender) {
-        return <div key={index}>No questions found!</div>
-      }
-    })
+    {props.questions.length === 0
+    ? <div>No questions found!</div>
+    : props.search.length >= 3
+    ? props.questions.some((question) => (
+      question.question_body.indexOf(props.search) > -1
+      ))
+      ? props.questions.map((question, index) => {
+          if (question.question_body.includes(props.search)) {
+            return <Question question={question} key={index}/>
+          }
+        })
+      : <div>No questions found!</div>
     : props.questions.map((question, index) => (
         <Question question={question} key={index}/>
     ))}
-    {props.questions.length === 0
-    ? <div>No questions found!</div>
-    : <></>
-    }
   </div>
 )
 
