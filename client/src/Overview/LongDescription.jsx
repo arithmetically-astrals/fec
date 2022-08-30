9e6cc27539e064d38be24756ad5223a88da965c3
Casey Penk <caseypenk@gmail.com>
2022-08-29 20:51:22 -0400

    implemented React hooks

--- client/src/Overview/LongDescription.jsx
+++ client/src/Overview/LongDescription.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
-const LongDescription = () => {
+const LongDescription = ({itemId = 37311}) => {
+  const [metaInfo, setmetaInfo] = useState(0)
+  const [starCount, setstarCount] = useState(0)
+
+  useEffect( () => {
+    axios.get('/products/item', {
+      params: {
+        product_id: itemId
+      }
+    }).then(response => {
+      setmetaInfo(response.data);
+      var totalStar = 0
+      var totalVal = 0
+      for (var key in response.data.ratings) {
+        totalStar += (Number(response.data.ratings[key]) * Number(key));
+        totalVal += Number(response.data.ratings[key]);
+      }
+      setstarRating((totalStar / totalVal).toFixed(1));
+      setstarCount(totalVal);
+    }).catch(err => {
+      console.log('err: ', err)
+    })
+  },[itemId])
+
   return (
-    <div id='overview-longdescription'>Long Product Description</div>
+    <div id='overview-longdescription'>{metaInfo.description}</div>
 )}
 
 export default LongDescription;
--- client/src/Overview/StylePicker.jsx
+++ client/src/Overview/StylePicker.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
-const StylePicker = () => {
+const StylePicker = ({itemId = 37311}) => {
+  const [metaInfo, setmetaInfo] = useState(0)
+  const [starCount, setstarCount] = useState(0)
+
+  useEffect( () => {
+    axios.get('/products/item', {
+      params: {
+        product_id: itemId
+      }
+    }).then(response => {
+      setmetaInfo(response.data);
+      var totalStar = 0
+      var totalVal = 0
+      for (var key in response.data.ratings) {
+        totalStar += (Number(response.data.ratings[key]) * Number(key));
+        totalVal += Number(response.data.ratings[key]);
+      }
+      setstarRating((totalStar / totalVal).toFixed(1));
+      setstarCount(totalVal);
+    }).catch(err => {
+      console.log('err: ', err)
+    })
+  },[itemId])
+
   return (
     <div id='overview-infopanel-stylepicker'>
       <div className='overview-infopanel-stylepicker-option'>
