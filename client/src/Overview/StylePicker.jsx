b2ead62ea689a22bedf393808b7f04e03b2fc313
Casey Penk <caseypenk@gmail.com>
2022-08-30 16:11:23 -0400

    updated styles

--- client/src/Overview/StylePicker.jsx
+++ client/src/Overview/StylePicker.jsx
@@ -2,8 +2,21 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 const StylePicker = ({itemId = 37311}) => {
-  const [metaInfo, setmetaInfo] = useState(0)
-  const [starCount, setstarCount] = useState(0)
+  const [styleInfo, setStyleInfo] = useState(0);
+  const [productInfo, setProductInfo] = useState(0)
+
+  useEffect( () => {
+    axios.get('/products/styles', {
+      params: {
+        product_id: itemId
+      }
+    }).then(response => {
+      setStyleInfo(response.data);
+      console.log('styles', response.data.results[0].name)
+    }).catch(err => {
+      console.log('err: ', err)
+    })
+  },[itemId])
 
   useEffect( () => {
     axios.get('/products/item', {
@@ -11,37 +24,44 @@ const StylePicker = ({itemId = 37311}) => {
         product_id: itemId
       }
     }).then(response => {
-      setmetaInfo(response.data);
-      var totalStar = 0
-      var totalVal = 0
-      for (var key in response.data.ratings) {
-        totalStar += (Number(response.data.ratings[key]) * Number(key));
-        totalVal += Number(response.data.ratings[key]);
-      }
-      setstarRating((totalStar / totalVal).toFixed(1));
-      setstarCount(totalVal);
+      setProductInfo(response.data);
+      console.log('styles', response.data.results[0].name)
     }).catch(err => {
       console.log('err: ', err)
     })
   },[itemId])
 
-  return (
-    <div id='overview-infopanel-stylepicker'>
-      <div className='overview-infopanel-stylepicker-styles'>
-        <div className='overview-infopanel-stylepicker-style'>
-          option 1
+
+  console.log(styleInfo)
+  if (styleInfo.results === undefined ) {
+    return <div>loading styles</div>
+  } else {
+    return (
+      <div id='overview-infopanel-stylepicker'>
+        <div id='overview-infopanel-stylepicker-styles'>
+          {styleInfo.results.map(({ name }) => (
+            <div className='overview-infopanel-stylepicker-style' key={name}>{name}</div>
+          ))}
+        </div>
+        <div id='overview-infopanel-stylepicker-sizeandquantity'>
+          {styleInfo.results.map(({ name }) => (
+            <div className='overview-infopanel-stylepicker-style' key={name}>{name}</div>
+          ))}
+          <select id='overview-infopanel-stylepicker-quantity'>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+          </select>
         </div>
       </div>
-      <div id='overview-infopanel-stylepicker-sizeandquantity'>
-        <div id='overview-infopanel-stylepicker-size'>
-          select size
-        </div>
-        <div id='overview-infopanel-stylepicker-quantity'>
-          1
-        </div>
-      </div>
-    </div>
-  )
+    )
+}
 }
 
 export default StylePicker;
