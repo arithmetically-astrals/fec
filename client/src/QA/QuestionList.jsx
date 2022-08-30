83186aefcc27106c822d7bab5c97f9ccfb4add68
Joseph <josephccchou@gmail.com>
2022-08-29 21:09:40 -0400

    minor formatting changes in QuestionList

--- client/src/QA/QuestionList.jsx
+++ client/src/QA/QuestionList.jsx
@@ -7,12 +7,12 @@ const QuestionList = (props) => (
     ? <div>No questions found!</div>
     : props.search.length >= 3
     ? props.questions.some((question) => (
-      question.question_body.indexOf(props.search) > -1
+        question.question_body.indexOf(props.search) > -1
       ))
       ? props.questions.map((question, index) => {
-          if (question.question_body.includes(props.search)) {
-            return <Question question={question} key={index}/>
-          }
+          if (question.question_body.includes(props.search)) (
+            <Question question={question} key={index}/>
+          )
         })
       : <div>No questions found!</div>
     : props.questions.map((question, index) => (
