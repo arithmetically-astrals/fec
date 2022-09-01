1c2c4370bbf32b5b18fedad6cc4c2e67e10335d8
Joseph <josephccchou@gmail.com>
2022-09-01 10:08:00 -0400

    display question headers side by side

--- client/src/QA/Question.jsx
+++ client/src/QA/Question.jsx
@@ -11,42 +11,50 @@ const Question = (props) => {
 
   return (
     <div>
-      Q: {props.question.question_body} {props.question.question_helpfulness === props.initialQuestionHelpfulness[props.question.question_id]
-      ? <a href="#" onClick={(e) => {
-          e.preventDefault();
-          axios.put(`/qa/questions/${props.question.question_id}/helpful`)
-            .then(() => {
-              axios.get('/qa/questions', {
-                params: {
-                  product_id: props.product_id,
-                  count: 10000
-                }
-              })
-                .then((response) => {
-                  props.setQuestions(response.data.results);
+      <div id='qa-question-container'>
+        <h3 id='qa-question-tag'>Q:</h3>
+        <div id='qa-question-body'>
+          {props.question.question_body}
+        </div>
+        <div id='qa-question-actions'>
+          {props.question.question_helpfulness === props.initialQuestionHelpfulness[props.question.question_id]
+          ? <a href="#" onClick={(e) => {
+              e.preventDefault();
+              axios.put(`/qa/questions/${props.question.question_id}/helpful`)
+                .then(() => {
+                  axios.get('/qa/questions', {
+                    params: {
+                      product_id: props.product_id,
+                      count: 10000
+                    }
+                  })
+                    .then((response) => {
+                      props.setQuestions(response.data.results);
+                    })
+                    .catch((err) => {
+                      console.log(err);
+                    });
                 })
                 .catch((err) => {
                   console.log(err);
                 });
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-      }}>Helpful?</a>
-      : <>Helpful!</>
-      } Yes({props.question.question_helpfulness}) | <AddAnswer question={props.question} product_id={props.product_id} setQuestions={props.setQuestions} initialAnswerHelpfulness={props.initialAnswerHelpfulness} setInitialAnswerHelpfulness={props.setInitialAnswerHelpfulness}/> | {reported
-      ? <>Reported</>
-      : <a href="#" onClick={(e) => {
-        e.preventDefault();
-        axios.put(`/qa/questions/${props.question.question_id}/report`)
-          .then(() => {
-            setReported(true);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }}>Report</a>
-      }
+          }}>Helpful?</a>
+          : <>Helpful!</>
+          } Yes({props.question.question_helpfulness}) | <AddAnswer question={props.question} product_id={props.product_id} setQuestions={props.setQuestions} initialAnswerHelpfulness={props.initialAnswerHelpfulness} setInitialAnswerHelpfulness={props.setInitialAnswerHelpfulness}/> | {reported
+          ? <>Reported</>
+          : <a href="#" onClick={(e) => {
+            e.preventDefault();
+            axios.put(`/qa/questions/${props.question.question_id}/report`)
+              .then(() => {
+                setReported(true);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          }}>Report</a>
+          }
+        </div>
+      </div>
       <AnswerList answers={Object.values(props.question.answers)} moreAnswers={moreAnswers} question_id={props.question.question_id} initialAnswerHelpfulness={props.initialAnswerHelpfulness} product_id={props.product_id} setQuestions={props.setQuestions}/>
       {Object.values(props.question.answers).length > 2
       ? <MoreAnswers moreAnswers={moreAnswers} setMoreAnswers={setMoreAnswers}/>
