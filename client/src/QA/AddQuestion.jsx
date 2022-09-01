150f292e4c8b7a63e6752768487e84d4945dfc6d
Joseph <josephccchou@gmail.com>
2022-09-01 15:33:11 -0400

    improve question modal

--- client/src/QA/AddQuestion.jsx
+++ client/src/QA/AddQuestion.jsx
@@ -13,65 +13,83 @@ const AddQuestion = (props) => {
     <>
       {questionModal}
       <button onClick={() => {
-        //have modal pop up with input body, name, email fields
         // BlurToggle();
         setQuestionModal(<div id='qa-question-modal'>
-          <h2>Ask Your Question</h2>
-          <h6>About the {props.productName}</h6>
+          <h2 id='qa-question-modal-header'>Ask Your Question</h2>
+          <h6 id='qa-question-modal-header'>About the {props.productName}</h6>
           <div>
             <div id='qa-question-modal-input'>
-              <label><b>Your Question* </b></label>
-              <input type='text' maxLength='1000' name='question'/>
+              <div>
+                <label><b>Your Question* </b></label>
+              </div>
+              <div>
+                <textarea id='qa-question-modal-textarea' maxLength='1000' rows="10" cols="80" onChange={(e) => {
+                  setBody(e.target.value);
+                }}/>
+              </div>
             </div>
             <div id='qa-question-modal-input'>
-              <label><b>What is your nickname* </b></label>
-              <input type='text' name='nickname' placeholder='Example: jackson11!'/>
+              <div>
+                <label><b>What is your nickname* </b></label>
+              </div>
+              <div>
+                <input type='text' placeholder='Example: jackson11!' onChange={(e) => {
+                  setName(e.target.value);
+                }}/>
+              </div>
               <div>
                 For privacy reasons, do not use your full name or email address
               </div>
             </div>
             <div id='qa-question-modal-input'>
-              <label><b>Your email* </b></label>
-              <input type='text' name='email' placeholder='Why did you like the product or not?'/>
+              <div>
+                <label><b>Your email* </b></label>
+              </div>
+              <div>
+                <input type='text' placeholder='Why did you like the product or not?' onChange={(e) => {
+                  setEmail(e.target.value);
+                }}/>
+              </div>
               <div>
                 For authentication reasons, you will not be emailed
               </div>
             </div>
-            <button onClick={(e) => {
-              console.log('hi')
+            <button id='qa-question-modal-button' onClick={(e) => {
+              console.log(body);
+              axios.post('/qa/questions', {
+                body: body,
+                name: name,
+                email: email,
+                product_id: props.product_id
+              })
+                .then(() => {
+                  axios.get('/qa/questions', {
+                    params: {
+                      product_id: props.product_id,
+                      count: 10000
+                    }
+                  })
+                    .then((response) => {
+                      let tempObj = props.initialQuestionHelpfulness;
+                      response.data.results.forEach((question) => {
+                        if (props.initialQuestionHelpfulness[question.question_id] === undefined) {
+                          tempObj[question.question_id] = question.question_helpfulness;
+                        }
+                      })
+                      props.setInitialQuestionHelpfulness(tempObj);
+                      props.setQuestions(response.data.results);
+                    })
+                    .catch((err) => {
+                      console.log(err);
+                    });
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
               }}>Submit</button>
           </div>
         </div>);
-        // axios.post('/qa/questions', {
-        //   body: 'trying something',
-        //   name: 'John Doe',
-        //   email: 'fakeemail@fakecompany.com',
-        //   product_id: props.product_id
-        // })
-        //   .then(() => {
-        //     axios.get('/qa/questions', {
-        //       params: {
-        //         product_id: props.product_id,
-        //         count: 10000
-        //       }
-        //     })
-        //       .then((response) => {
-        //         let tempObj = props.initialQuestionHelpfulness;
-        //         response.data.results.forEach((question) => {
-        //           if (props.initialQuestionHelpfulness[question.question_id] === undefined) {
-        //             tempObj[question.question_id] = question.question_helpfulness;
-        //           }
-        //         })
-        //         props.setInitialQuestionHelpfulness(tempObj);
-        //         props.setQuestions(response.data.results);
-        //       })
-        //       .catch((err) => {
-        //         console.log(err);
-        //       });
-        //   })
-        //   .catch((err) => {
-        //     console.log(err);
-        //   });
+
       }}>Add a question</button>
     </>
   )
