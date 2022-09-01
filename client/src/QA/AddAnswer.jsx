ae0521725bf4c5af64734195890edcd2fb83a3d0
Joseph <josephccchou@gmail.com>
2022-09-01 12:28:32 -0400

    add states to AddAnswer.jsx

--- client/src/QA/AddAnswer.jsx
+++ client/src/QA/AddAnswer.jsx
@@ -1,43 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 
-const AddAnswer = (props) => (
-  <a href="#" onClick={(e) => {
-    e.preventDefault();
-    //modal popup with body, name, email, photos
-    axios.post(`/qa/questions/${props.question.question_id}/answers`, {
-      body: 'temporary answer',
-      name: 'John Doe',
-      email: 'fakeemail@fakecompany.com',
-      photos: []
-    })
-      .then(() => {
-        axios.get('/qa/questions', {
-          params: {
-            product_id: props.product_id,
-            count: 10000
-          }
-        })
-          .then((response) => {
-            let tempObj = props.initialAnswerHelpfulness;
-            response.data.results.forEach((question) => {
-              Object.keys(question.answers).forEach((id) => {
-                if (props.initialAnswerHelpfulness[id] === undefined) {
-                  tempObj[id] = question.answers[id].helpfulness;
-                }
-              })
-            })
-            props.setInitialAnswerHelpfulness(tempObj);
-            props.setQuestions(response.data.results);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+const AddAnswer = (props) => {
+
+  const [body, setBody] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [photos, setPhotos] = useState('');
+
+  return (
+    <a href="#" onClick={(e) => {
+      e.preventDefault();
+      //modal popup with body, name, email, photos
+      axios.post(`/qa/questions/${props.question.question_id}/answers`, {
+        body: 'temporary answer',
+        name: 'John Doe',
+        email: 'fakeemail@fakecompany.com',
+        photos: []
       })
-      .catch((err) => {
-        console.log(err);
-      });
-  }}>Add Answer</a>
-)
+        .then(() => {
+          axios.get('/qa/questions', {
+            params: {
+              product_id: props.product_id,
+              count: 10000
+            }
+          })
+            .then((response) => {
+              let tempObj = props.initialAnswerHelpfulness;
+              response.data.results.forEach((question) => {
+                Object.keys(question.answers).forEach((id) => {
+                  if (props.initialAnswerHelpfulness[id] === undefined) {
+                    tempObj[id] = question.answers[id].helpfulness;
+                  }
+                })
+              })
+              props.setInitialAnswerHelpfulness(tempObj);
+              props.setQuestions(response.data.results);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }}>Add Answer</a>
+  )
+}
 
 export default AddAnswer;
--- client/src/QA/Answer.jsx
+++ client/src/QA/Answer.jsx
@@ -1,8 +1,6 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 
-// var tempAnswerStorage = {};
-
 const Answer = (props) => {
 
   const [reported, setReported] = useState(false);
