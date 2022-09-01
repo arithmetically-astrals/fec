d04bfdc7da2921a21a1ca9f2eee93899619f523c
Daniel Elliott <daniel.s.elliott.33@gmail.com>
2022-09-01 10:26:52 -0500

    arrows on carousel are now functional, disappear when at end of each side of carousel

--- client/src/Related/RelatedList/RelatedList.jsx
+++ client/src/Related/RelatedList/RelatedList.jsx
@@ -8,46 +8,44 @@ const RelatedList = ({defaultData, productList}) => {
   const [showRight, setShowRight] = useState(true);
   const [slideLeft, setSlideLeft] = useState(0);
 
-  // const scrollRef = useRef();
+  const scrollRef = useRef();
 
 
-  // const handleScrollLeft = () => {
-    // setShowRight(true);
-    // if (slideLeft - 200 < 0) {
-    //   var leftover = 200 - slideLeft;
-    // } else {
-    //   var leftover = 200;
-    // }
-    // scrollRef.current.scrollLeft -= leftover;
-    // setSlideLeft((scrollRef.current.scrollLeft -= leftover));
-  // }
+  const handleScrollLeft = () => {
+    setShowRight(true);
+    if (slideLeft - 200 <= 0) {
+      var leftover = 200 - slideLeft;
+    } else {
+      var leftover = 200;
+    }
+    scrollRef.current.scrollLeft -= leftover;
+    setSlideLeft((scrollRef.current.scrollLeft -= leftover));
+  }
 
-  // const handleScrollRight = () => {
-    // const width = scrollRef.current.scrollWidth - scrollRef.current.clientWidth;
+  const handleScrollRight = () => {
+    const width = scrollRef.current.scrollWidth - scrollRef.current.clientWidth;
 
-    // if (slideLeft + 200 > width) {
-    //   setShowRight(false);
-    //   var leftover = slideLeft + 300 - width;
-    // } else {
-    //   var leftover = 200;
-    // }
-    // scrollRef.current.scrollLeft += leftover;
-    // setSlideLeft((scrollRef.current.scrollLeft += leftover));
-// }
+    if (slideLeft + 200 >= width) {
+      setShowRight(false);
+      var leftover = slideLeft + 300 - width;
+    } else {
+      var leftover = 200;
+    }
+    scrollRef.current.scrollLeft += leftover;
+    setSlideLeft((scrollRef.current.scrollLeft += leftover));
+}
 
   return (
     <div id='related-card-container'>
-      {/* {slideLeft <  0 ? (<div id='related-left-arrow' onClick={handleScrollLeft}>&#8592;</div>) : ('')} */}
-      <div id='related-left-arrow'>&#8592;</div>
+      {slideLeft >  0 ? (<div id='related-left-arrow' onClick={() => {handleScrollLeft();}}>&#8592;</div>) : ('')}
 
-      <div id='related-card-list' /*ref={scrollRef}*/>
+      <div id='related-card-list' ref={scrollRef}>
         {productList.map((product) => {
-          return <RelatedListItem key={product.id} itemId={product.id} product={product} defaultData={defaultData} /*colorScheme={colorScheme}*//>
+          return <RelatedListItem key={product.id} itemId={product.id} product={product} defaultData={defaultData} />
         })}
       </div>
 
-      {/* {showRight ? (<div id='related-right-arrow' onClick={handleScrollRight()}>&#8594;</div>) : ('')} */}
-      <div id='related-right-arrow'>&#8594;</div>
+      {showRight ? (<div id='related-right-arrow' onClick={() => {handleScrollRight()}}>&#8594;</div>) : ('')}
 
     </div>
 
