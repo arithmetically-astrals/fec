5e3ba9db5dc1ac3229e9f609aab9758ee211b065
Brian Vose <103958999+Banzubie@users.noreply.github.com>
2022-09-01 13:56:53 -0600

    review form almost complete, added rating menu and remaining text counter

--- client/src/Reviews/ReviewForm.jsx
+++ client/src/Reviews/ReviewForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import ReviewSelector from './ReviewRatingSelectors.jsx';
 
 //renders the write new review form
 const ReviewForm = ({itemId, setWriteReview, metaInfo}) => {
@@ -7,10 +8,8 @@ const ReviewForm = ({itemId, setWriteReview, metaInfo}) => {
   const [reccomend, setReccommend] = useState(false);
   const [mainRating, setMainRating] = useState(0);
   const [photoUpload, setPhotoUpload] = useState([]);
-
-  const closeReview = () => {
-    setWriteReview(false);
-  }
+  const [charRatings, setCharRatings] = useState({});
+  const [reviewBody, setReviewBody] = useState('');
 
   const submitReview = (e) => {
     e.preventDefault();
@@ -27,17 +26,10 @@ const ReviewForm = ({itemId, setWriteReview, metaInfo}) => {
       name: e.target.username.value,
       email: e.target.email.value,
       photos: photoUpload,
-      characteristics: {
-        Comfort: 5,
-        Fit: 4,
-        Length: 3,
-        Quality: 2,
-        Size: 1,
-        Width: 0
-      }
+      characteristics: charRatings
     }).then(response => {
       console.log('Recieved :', response.data)
-    })
+    }).catch(err => console.log('post review error:', err))
     //closeReview();
   }
 
@@ -48,101 +40,32 @@ const ReviewForm = ({itemId, setWriteReview, metaInfo}) => {
     setPhotoUpload(photoCopy)
   }
 
-  const reviewStarScale = (num) => {
-    var textRating = ' - Choose a star'
-    if (mainRating === 1) {
-      textRating = ' - Poor'
-    } else if (mainRating === 2) {
-      textRating = ' - Fair'
-    } else if (mainRating === 3) {
-      textRating = ' - Average'
-    } else if (mainRating === 4) {
-      textRating = ' - Good'
-    } else if (mainRating === 5) {
-      textRating = ' - Great'
-    }
-    var rating = {
-      float: 'left',
-      unicodeBidi: 'bidi-override',
-      color: '#c5c5c5',
-      fontSize: '15px',
-      height: '15px',
-      width: '75px',
-      margin: '0 auto',
-      position: 'relative',
-      padding: 0,
-      textShadow: '0px 1px 0 #a2a2a2'
-    }
+  const clickCharRating = (e) => {
+    var charsCopy = Object.assign({}, charRatings);
+    charsCopy[e.target.name] = e.target.value;
+    setCharRatings(charsCopy)
+  }
 
-    var ratingUpper = {
-      width: Math.round((num / 5) * 100) + '%',
-      color: '#e7711b',
-      padding: 0,
-      position: 'absolute',
-      zIndex: 1,
-      display: 'flex',
-      top: 0,
-      left: 0,
-      overflow: 'hidden',
-      cursor: 'pointer'
-    }
-
-    var ratingLower = {
-      padding: 0,
-      display: 'flex',
-      zIndex: 0,
-      cursor: 'pointer'
-    }
-    return (
-      <div>
-        <div style={rating}>
-          <div style={ratingUpper}>
-              <span onClick={() => setMainRating(1)}>★</span>
-              <span onClick={() => setMainRating(2)}>★</span>
-              <span onClick={() => setMainRating(3)}>★</span>
-              <span onClick={() => setMainRating(4)}>★</span>
-              <span onClick={() => setMainRating(5)}>★</span>
-          </div>
-          <div style={ratingLower}>
-              <span onClick={() => setMainRating(1)}>★</span>
-              <span onClick={() => setMainRating(2)}>★</span>
-              <span onClick={() => setMainRating(3)}>★</span>
-              <span onClick={() => setMainRating(4)}>★</span>
-              <span onClick={() => setMainRating(5)}>★</span>
-          </div>
-        </div>
-        <span>{textRating}</span>
-      </div>
-    )
+  const bodyChange = (e) => {
+    setReviewBody(e.target.value);
   }
 
   return (
     <div id='review-write-form'>
-      <span onClick={closeReview} style={{cursor: 'pointer', float: 'right'}}>X</span>
+      <span onClick={() => setWriteReview(false)} style={{cursor: 'pointer', float: 'right'}}>X</span>
       <h3>Write Your Review</h3>
       <h5>About the [Product Name Here]</h5>
-      <div style={{float: 'right'}}>
-        <span>Overall rating</span>
-          <div>
-          {reviewStarScale(mainRating)}
-          </div>
-        <br/>
-        <span>Do you reccomend this product?</span>
-        <br/>
-        Yes<input name='recommend' type='radio' onClick={() => setReccommend(true)} required/>
-        No <input name='recommend' type='radio' onClick={() => setReccommend(false)} required/>
-        <br/>
-      </div>
+      <ReviewSelector metaInfo={metaInfo} mainRating={mainRating} setMainRating={setMainRating} setReccommend={setReccommend} clickCharRating={clickCharRating}/>
       <form onSubmit={submitReview}>
-        <span>Size, Width, Comfort, Quality, Length, Fit ratings</span>
-        <br/>
         Summary:
         <br/>
         <input type='text' name='summary' maxLength='60' style={{width: '400px'}} />
         <br/>
         Review:
         <br/>
-        <textarea name='body' minLength='50' maxLength='1000' style={{width: '400px', height: '250px', resize: 'none'}} required />
+        <textarea name='body' minLength='50' maxLength='1000' style={{width: '400px', height: '250px', resize: 'none'}} required onChange={bodyChange}/>
+        <br/>
+        {reviewBody.length < 50 ? <p style={{fontSize: 'x-small'}}>Minimum required characters left: {50 - reviewBody.length}</p> : <p style={{fontSize: 'x-small'}}>Minimum reached</p>}
         <br/>
         {photoUpload.map(photo => {
           return (
@@ -160,7 +83,7 @@ const ReviewForm = ({itemId, setWriteReview, metaInfo}) => {
         <br/>
         Email: <input type='email' name='email' maxLength='60' placeholder='Example: jackson11@email.com' required style={{width: '250px'}}></input>
         <br/>
-        <input type='submit' style={{float: 'right'}}></input>
+        <input type='submit' style={{cursor: 'pointer'}}></input>
       </form>
     </div>
   )
