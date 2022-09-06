ab86ef635a307eb676e7ef18f31e83db517547a2
Daniel Elliott <daniel.s.elliott.33@gmail.com>
2022-09-06 15:50:49 -0500

    fixed duplicate related products bug

--- client/src/Related/Related.jsx
+++ client/src/Related/Related.jsx
@@ -24,13 +24,14 @@ function Related({itemId, starRating, setitemId}) {
     })  //...then set default data state with response data
       .then(overviewData => {
         setDefaultData(overviewData.data);
-        //get related items to current product
+        //GET related items to current product
         axios.get(`/products/relatedlist`, {
           params: {
             product_id: itemId
           }
         })//...then map relatedList items, converting relatedItems array into Promise
           .then((relatedList) => {
+            console.log('related list', relatedList)
             Promise.all(
               relatedList.data.map((relatedListItem) =>
                 axios.get(`/products/item`, {
@@ -38,20 +39,19 @@ function Related({itemId, starRating, setitemId}) {
                     product_id: relatedListItem
                   }
                 })
-                  .then((relatedListItemResponse) => relatedListItemResponse.data)),//end mapping
-            )//end of Promise.all, now set state of ProductList with relatedList
+                  .then((relatedListItemResponse) => relatedListItemResponse.data)), //end mapping
+            )//end of Promise.all, now filter duplicates from related list
               .then((prodList) => {
-              //   //filtering duplicates from the productList
-              //   const uniqueList = [];
-              //   const uniqueProducts = prodList.filter(eachProduct => {
-              //     const isDuplicate = uniqueList.includes(eachProduct.id);
-              //     if(!isDuplicate) {uniqueList.push(eachProduct);
-              //     return true;
-              //     }
-              // })
-              // setProductList(uniqueList);
-            // })
-                setProductList(prodList);
+                const uniqueList = [];
+                const uniqueProducts = prodList.filter(eachProduct => {
+                  const isDuplicate = uniqueList.includes(eachProduct.id);
+                  if(!isDuplicate) {
+                    uniqueList.push(eachProduct.id);
+                    return true;
+                  }
+                })
+                //set state of ProductList with new unique relatedList
+                setProductList(uniqueProducts);
               })
               .catch(err => {
                 console.log(err);
@@ -62,7 +62,7 @@ function Related({itemId, starRating, setitemId}) {
 
     if(!productList || !defaultData) {
       return (
-        <div>Loading reviews...</div>
+        <div>Loading Related Products...</div>
       )
     } else {
       return (
