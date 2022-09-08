ce7f060de42623201739acc038431615944a57e8
Joseph <josephccchou@gmail.com>
2022-09-08 12:34:30 -0400

    comment out unnecessary server path

--- server/index.js
+++ server/index.js
@@ -257,20 +257,20 @@ app.put('/qa/questions/:question_id/helpful', (req, res) => {
 });
 
 //report a question
-app.put('/qa/questions/:question_id/report', (req, res) => {
-  axios.put(`${process.env.API}/qa/questions/${req.params.question_id}/report`, null, {
-    headers: {
-      Authorization: process.env.AUTH_CODE
-    }
-  })
-    .then(() => {
-      res.sendStatus(204);
-    })
-    .catch((err) => {
-      console.log(err.response.data);
-      res.sendStatus(501);
-    });
-});
+// app.put('/qa/questions/:question_id/report', (req, res) => {
+//   axios.put(`${process.env.API}/qa/questions/${req.params.question_id}/report`, null, {
+//     headers: {
+//       Authorization: process.env.AUTH_CODE
+//     }
+//   })
+//     .then(() => {
+//       res.sendStatus(204);
+//     })
+//     .catch((err) => {
+//       console.log(err.response.data);
+//       res.sendStatus(501);
+//     });
+// });
 
 //mark an answer as helpful
 app.put('/qa/answers/:answer_id/helpful', (req, res) => {
